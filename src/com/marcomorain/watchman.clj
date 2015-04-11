df4295beabc841398e5b1ee79e53f9e552ce4f01
Marc O'Morain <github.com@marcomorain.com>
2015-04-11 20:46:27 +0100

    get-sockname

--- src/com/marcomorain/watchman.clj
+++ src/com/marcomorain/watchman.clj
@@ -1,4 +1,13 @@
-(ns com.marcomorain.watchman)
+(ns com.marcomorain.watchman
+  (require
+    [clojure.java.shell :as sh]
+    [cheshire.core :refer :all]))
+
+(defn- get-sockname []
+  (-> (sh/sh "watchman" "get-sockname")
+      :out
+      (parse-string true)
+      :sockname))
 
 (defn foo
   "I don't do a whole lot."
