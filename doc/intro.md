361d3c27aff075851509e91f698e99adf577eec4
Marc O'Morain <github.com@marcomorain.com>
2015-04-11 20:36:04 +0100

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to watchman
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/marcomorain/watchman_test.clj
@@ -0,0 +1,7 @@
+(ns com.marcomorain.watchman-test
+  (:require [clojure.test :refer :all]
+            [com.marcomorain.watchman :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
