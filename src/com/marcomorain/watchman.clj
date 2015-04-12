9af6e8d515c9d33e9a84ffdddc6b26c50eb0e3ae
Marc O'Morain <github.com@marcomorain.com>
2015-04-12 22:26:45 +0100

    Make a watchman client

--- src/com/marcomorain/watchman.clj
+++ src/com/marcomorain/watchman.clj
@@ -1,7 +1,26 @@
 (ns com.marcomorain.watchman
-  (require
-    [clojure.java.shell :as sh]
-    [cheshire.core :refer :all]))
+  (require [clojure.java.shell :as sh]
+           [cheshire.core :refer :all]
+           [clojure.tools.logging :refer (infof)]
+           )
+  (use [clojure.java.io :as io])
+  (import [jnr.unixsocket UnixSocketAddress UnixSocketChannel]
+          [java.io PrintWriter InputStreamReader BufferedReader]
+          [java.nio.channels Channels]
+          [java.nio CharBuffer]))
+
+(defn execute-command [watchman command]
+  (let [json (str (generate-string command) \newline)]
+    (doto (:writer watchman)
+      (.print json)
+      (.flush)))
+  (let [response (-> watchman
+                     :reader
+                     .readLine
+                     (parse-string true))]
+    (when (contains? response :error)
+      (throw (IllegalArgumentException. (:error response))))
+    response))
 
 (defn- get-sockname []
   (-> (sh/sh "watchman" "get-sockname")
@@ -9,7 +28,29 @@
       (parse-string true)
       :sockname))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn connect
+  ([]
+   (connect (get-sockname)))
+  ([sockname]
+   (let [path (io/file sockname)
+         address (UnixSocketAddress. path)
+         channel (UnixSocketChannel/open address)
+         writer (PrintWriter. (Channels/newOutputStream channel))
+         input (InputStreamReader. (Channels/newInputStream channel))
+         reader (BufferedReader. input)]
+     (infof "Connected to %s" sockname)
+     {:reader reader
+      :writer writer})))
+
+;; Commands - make these from a macro
+(defn get-config [watchman path]
+  (execute-command watchman ["get-config" path]))
+
+(defn clock [watchman path]
+  (execute-command watchman ["clock" path]))
+
+(defn log-level [watchman level]
+  (execute-command watchman ["log-level" level]))
+
+(defn version [watchman]
+  (execute-command watchman ["version"]))
